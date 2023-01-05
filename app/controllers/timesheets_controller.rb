7c597c3a329e6da71ef91c9f2577f25360ea7ae3
Nicholas Mendez <mendez.nicholasr@gmail.com>
2023-01-05 12:50:47 -0500

    created controller actions for timesheet table/model

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -1,6 +1,18 @@
 class TimesheetsController < ApplicationController
 
   def index
-    
+    timesheets = Timesheet.all
+    render json: timesheets, status: :ok
+  end
+
+  def create
+    timesheet_entry = Timesheet.create(timesheet_params)
+    render json: timesheet_entry, status: :created
+  end
+
+  private
+
+  def timesheet_params
+    params.permit(:billing_date, :client, :project, :project_code, :hours, :is_billable, :first_name, :last_name, :billable_rate)
   end
 end
