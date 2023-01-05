f5bd4a5075105cb675d4968e03ad30088fef2c3b
Nicholas Mendez <mendez.nicholasr@gmail.com>
2023-01-05 11:28:20 -0500

    created form to accept entries for timesheet , will style upon validating functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import './styles/App.css';
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
 import CsvForm from './components/CsvForm';
+
+import logo from './assets/billit-logo.png'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/App.css';
 function App() {
   return (
     <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand className='py-0'>
+            <img
+              alt=""
+              src={logo}
+              width="auto"
+              height="30"
+              className="d-inline-block align-top"
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
       <CsvForm />
     </div>
   );
--- client/src/components/CsvForm.jsx
+++ client/src/components/CsvForm.jsx
@@ -1,15 +1,39 @@
 import React,{useState} from 'react'
 import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
 
 const CsvForm = () => {
   const [CsvFile, setCsvFile] = useState()
 
   return (
-      <Container className='pt-5'>
-        <input 
-        type="file" 
-        accept='.csv'
-        />
+      <Container className='py-2 my-4 border border-3 rounded w-50'>
+        <h2 className='text-center'>New Timesheet Entry</h2>
+        <Form className="bg-white">
+          <Form.Control type="text" placeholder="Client..." className='underline-text-input mb-2'/>
+          <Form.Control type="text" placeholder="Project name..." className='underline-text-input mb-2'/>
+          <Form.Control type="text" placeholder="Project code..."className='underline-text-input mb-2'/>
+          <Form.Control type="text" placeholder="First name..."className='underline-text-input mb-2'/>
+          <Form.Control type="text" placeholder="Last name..."className='underline-text-input mb-2'/>
+          <Form.Group>
+            <Form.Label>Is Billable?</Form.Label>
+            <Form.Select>
+              <option value={1}>Yes</option>
+              <option value={0}>No</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Hours</Form.Label>
+            <Form.Control type="number" step={0.01}/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Billable Rate</Form.Label>
+            <Form.Control type="number" step={0.01}/>
+          </Form.Group>
+          {/* <input 
+          type="file" 
+          accept='.csv'
+          /> */}
+        </Form>
       </Container>
   )
 }
