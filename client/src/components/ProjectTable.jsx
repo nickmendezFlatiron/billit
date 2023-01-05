deebc2ac5b24c991a84ad344fb662bd461523557
Nicholas Mendez <mendez.nicholasr@gmail.com>
2023-01-05 12:15:27 -0500

    styled table header for ProjectTable component

--- client/src/components/ProjectTable.jsx
+++ client/src/components/ProjectTable.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Table from 'react-bootstrap/Table';
 
 const ProjectTable = () => {
   return (
-    <div>ProjectTable</div>
+    <div>
+      <Table>
+        <thead hover>
+          <tr className='table-header'>
+            <th className='ps-2'>Name</th>
+            <th>Clients</th>
+            <th className='text-end'>Hours</th>
+            <th className='text-end'>Billable Hours</th>
+            <th className='text-end pe-2'>Billable Amount</th>
+          </tr>
+        </thead>
+      </Table>
+    </div>
   )
 }
 
