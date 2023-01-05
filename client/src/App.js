35e027932f722bc120bd820f8ed7ed2a0f0b2bb5
Nicholas Mendez <mendez.nicholasr@gmail.com>
2023-01-04 22:33:35 -0500

    imported bootstrap css styling to app component and appended input tag to accept csv files within CsvForm component

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './styles/App.css';
 import CsvForm from './components/CsvForm';
 function App() {
--- client/src/components/CsvForm.jsx
+++ client/src/components/CsvForm.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Container from 'react-bootstrap/Container'
 
 const CsvForm = () => {
+  const [CsvFile, setCsvFile] = useState()
+
   return (
-    <div>CsvForm</div>
+      <Container className='pt-5'>
+        <input 
+        type="file" 
+        accept='.csv'
+        />
+      </Container>
   )
 }
 
